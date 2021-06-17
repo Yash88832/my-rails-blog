f1f25384214e5115d557191aa8c230838bf3b9b8
YashRathore <yash.r@geitpl.work>
2021-06-18 02:01:32 +0530

    integrate controller of posts and html pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,13 +8,15 @@ class Post < ApplicationRecord
   has_many :post_tags
   has_many :tags, through: :post_tags
 
-  validates :title, :body, :snippet, :status, presence: true
+  validates :title, :body, :snippet, :status, presence: true  
+  scope :published, -> { where(status: [STATUS_PUBLISHED]) }
+  scope :archived, -> { where(status: [STATUS_ARCHIVED]) }
   before_save :set_published_date
 
   private
 
   def set_published_date
-    if !self.published_date && [STATUS_PUBLISHED].include?(status)
+    if STATUS_PUBLISHED.eql?(status)
       self.published_date = Time.now
     end
   end
